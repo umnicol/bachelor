85f33e1187b64efa93f42098749a1f4376db56e5
umnicol <nicoltomms@gmail.com>
2023-12-04 19:09:24 +0100

    routing

---
+++ fitness-app/src/app/signin/page.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import SignInForm from '../components/SignInForm/SignInForm'
+
+export default function SignIn() {
+  return (
+    <>
+      <Head>
+        <title>Laurafit - Sign In</title>
+        <meta name="description" content="nieco sem" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      
+      <SignInForm/>
+
+    </>
+  )
+}
