466e1e680bf085dce9fc8e3d91eb769d46cb6d6f
umnicol <nicoltomms@gmail.com>
2024-01-07 21:21:05 +0100

    1

--- fitness-app/src/app/components/HeaderMainPage/HeaderMainPage.tsx
+++ fitness-app/src/app/components/HeaderMainPage/HeaderMainPage.tsx
@@ -13,7 +13,7 @@ const HeaderMainPage: React.FC<HeaderMainPageProps> = ({image = '/img-signin.png
 
  return (
     <header className={styles.mainpageheader} style={{ backgroundImage: `url(${image})` }}>
-         <h2 className={styles.mainpageheader_h2}>DON'T WAIT</h2>
+         <h2 className={styles.mainpageheader_h2}>DONT WAIT</h2>
          <h2 className={styles.mainpageheader_h2_1}>START TODAY</h2>
 
       <Link href="/preview/https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Flaurafit-3d0b4.appspot.com%2Fo%2Fsculpt-it.jpg%3Falt%3Dmedia%26token%3Dacac9ee9-f74f-4452-ba42-cca14d11bcea">
